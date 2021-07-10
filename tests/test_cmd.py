b7984c2cca3c77d9851d28cf2738bb35ffda06b4
Michal Dabski <michaldabski@gmail.com>
2021-07-11 00:15:53 +0100

    Implement tests

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,18 +1,30 @@
 from unittest import TestCase
 
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
 
 from poeditor_sync.cmd import poeditor
 
 
-class CmdTest(TestCase):
-
+class CmdReadOnlyTokenTest(TestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.runner = CliRunner()
+        self.runner = CliRunner(env={
+            'POEDITOR_CONFIG_FILE': 'test.yml',
+        })
 
     def test_poeditor(self):
-        self.runner.invoke(poeditor)
+        result: Result = self.runner.invoke(poeditor)
+        self.assertEqual(result.exit_code, 0)
+        self.assertTrue(result.stdout.startswith('Usage: poeditor'))
 
     def test_poeditor_pull(self):
-        self.runner.invoke(poeditor, 'pull')
+        result: Result = self.runner.invoke(poeditor, ['pull'])
+        self.assertEqual(result.exit_code, 0, result.stdout)
+
+    def test_poeditor_push(self):
+        result: Result = self.runner.invoke(poeditor, 'push')
+        self.assertEqual(result.exit_code, 1)
+
+    def test_poeditor_push_terms(self):
+        result: Result = self.runner.invoke(poeditor, 'push')
+        self.assertEqual(result.exit_code, 1)
